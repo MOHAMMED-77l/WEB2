f0a7342643da3904b3238273d7f147a64749e63c
MOHAMMED-77l <hmsy0594@gmail.com>
2026-04-09 13:27:54 +0100

    Update calculate.php

--- lab1/calculate.php
+++ lab1/calculate.php
@@ -1,70 +1,54 @@
 <?php
 header('Content-Type: application/json');
+include 'db.php';
 
-if (isset($_POST['course'], $_POST['credits'], $_POST['grade'])) {
+if (isset($_POST['course'])) {
 
-    $courses = $_POST['course'];
-    $credits = $_POST['credits'];
-    $grades = $_POST['grade'];
+$student = $_POST['student_name'];
+$semester = $_POST['semester'];
 
-    $totalPoints = 0;
-    $totalCredits = 0;
+$courses = $_POST['course'];
+$credits = $_POST['credits'];
+$grades = $_POST['grade'];
 
-    $tableHtml = '<table class="table table-bordered mt-3">';
-    $tableHtml .= '<thead class="thead-dark">
-    <tr>
-    <th>Course</th><th>Credits</th>
-    <th>Grade</th><th>Points</th>
-    </tr></thead><tbody>';
+$totalPoints = 0;
+$totalCredits = 0;
+
+for ($i = 0; $i < count($courses); $i++) {
+    $cr = floatval($credits[$i]);
+    $g = floatval($grades[$i]);
+
+    if ($cr <= 0) continue;
+
+    $totalPoints += $cr * $g;
+    $totalCredits += $cr;
+}
+
+if ($totalCredits > 0) {
+
+    $gpa = $totalPoints / $totalCredits;
+
+    $stmt = $conn->prepare("INSERT INTO results (student_name, semester, gpa) VALUES (?, ?, ?)");
+    $stmt->bind_param("ssd", $student, $semester, $gpa);
+    $stmt->execute();
+
+    $result_id = $stmt->insert_id;
 
     for ($i = 0; $i < count($courses); $i++) {
-        $course = htmlspecialchars($courses[$i]);
-        $cr = floatval($credits[$i]);
-        $g = floatval($grades[$i]);
-
-        if ($cr <= 0) continue;
-
-        $pts = $cr * $g;
-        $totalPoints += $pts;
-        $totalCredits += $cr;
-
-        $tableHtml .= "<tr>
-        <td>$course</td>
-        <td>$cr</td>
-        <td>$g</td>
-        <td>$pts</td>
-        </tr>";
+        $stmt2 = $conn->prepare("INSERT INTO courses (result_id, course_name, credits, grade) VALUES (?, ?, ?, ?)");
+        $stmt2->bind_param("isdd", $result_id, $courses[$i], $credits[$i], $grades[$i]);
+        $stmt2->execute();
     }
 
-    $tableHtml .= '</tbody></table>';
-
-    if ($totalCredits > 0) {
-        $gpa = $totalPoints / $totalCredits;
-
-        if ($gpa >= 3.7) $interpretation = "Distinction";
-        elseif ($gpa >= 3.0) $interpretation = "Merit";
-        elseif ($gpa >= 2.0) $interpretation = "Pass";
-        else $interpretation = "Fail";
-
-        $message = "Your GPA is " . number_format($gpa, 2) . " ($interpretation)";
-
-        echo json_encode([
-            'success' => true,
-            'gpa' => $gpa,
-            'message' => $message,
-            'tableHtml' => $tableHtml
-        ]);
-    } else {
-        echo json_encode([
-            'success' => false,
-            'message' => 'No valid courses'
-        ]);
-    }
+    echo json_encode([
+        'success' => true,
+        'gpa' => $gpa,
+        'message' => "Saved! GPA = " . number_format($gpa, 2)
+    ]);
 
 } else {
-    echo json_encode([
-        'success' => false,
-        'message' => 'No data received'
-    ]);
+    echo json_encode(['success' => false, 'message' => 'Invalid data']);
+}
+
 }
 ?>
